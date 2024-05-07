16ec5b3fd769dd1d01b81ab50e2fce412c997267
dieg0zu <106353782+dieg0zu@users.noreply.github.com>
2024-05-07 16:26:03 -0500

    Delete Proyecto directory

--- Proyecto/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var formularioLogin = document.getElementById('formulario-login');
-
-    formularioLogin.addEventListener('submit', function(event) {
-        event.preventDefault();
-        window.location.href = './opciones/cursos.html'; 
-    });
-});
